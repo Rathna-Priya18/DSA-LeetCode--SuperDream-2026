e767ed3d1f84fdd2faef499438ee2914f34ed018
Rathna-Priya18 <rathnapriya421@gmail.com>
2026-09-25 20:23:06 +0530

    Time: 3 ms (15.8%), Space: 43.9 MB (31.16%) - LeetHub

---
+++ 1652-defuse-the-bomb/1652-defuse-the-bomb.java
@@ -0,0 +1,23 @@
+class Solution {
+    public int[] decrypt(int[] code, int k) {
+        int n = code.length;
+        int r[] = new int[n];
+        if(k==0){
+            return r;
+        }
+
+        for(int i = 0;i<n;i++){
+            if(k>0){
+                for(int j=i+1;j<i+1+k;j++){
+                    r[i] += code[j%n];
+                }
+            }
+            else if(k<0){
+                for(int j =i-1;j>i-1-Math.abs(k);j--){
+                    r[i] += code[((j % n) + n) % n];
+                }
+            }
+        }
+        return r;
+    }
+}
